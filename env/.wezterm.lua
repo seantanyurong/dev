f8c823ce9678df9b1c7fe9a943d6e1a37e9ad181
Sean Tan <seantanyurong@gmail.com>
2025-07-08 14:12:07 +0800

    feature: initial setup

---
+++ env/.wezterm.lua
@@ -0,0 +1,96 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+config.color_scheme = "Tokyo Night"
+
+config.font = wezterm.font("JetBrainsMono Nerd Font")
+config.font_size = 15
+
+config.window_decorations = "RESIZE"
+
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 40
+
+config.leader = { key = "s", mods = "CTRL", timeout_milliseconds = 1000 }
+
+local function move_pane(key, direction)
+	return {
+		key = key,
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection(direction),
+	}
+end
+
+local function resize_pane(key, direction)
+	return {
+		key = key,
+		action = wezterm.action.AdjustPaneSize({ direction, 3 }),
+	}
+end
+
+config.keys = {
+	-- ... add these new entries to your config.keys table
+	{
+		-- I'm used to tmux bindings, so am using the quotes (") key to
+		-- split horizontally, and the percent (%) key to split vertically.
+		key = '"',
+		-- Note that instead of a key modifier mapped to a key on your keyboard
+		-- like CTRL or ALT, we can use the LEADER modifier instead.
+		-- This means that this binding will be invoked when you press the leader
+		-- (CTRL + A), quickly followed by quotes (").
+		mods = "LEADER",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "%",
+		mods = "LEADER",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "w",
+		mods = "LEADER",
+		action = wezterm.action.CloseCurrentPane({ confirm = true }),
+	},
+	{
+		key = "a",
+		-- When we're in leader mode _and_ CTRL + A is pressed...
+		mods = "LEADER|CTRL",
+		-- Actually send CTRL + A key to the terminal
+		action = wezterm.action.SendKey({ key = "a", mods = "CTRL" }),
+	},
+	move_pane("j", "Down"),
+	move_pane("k", "Up"),
+	move_pane("h", "Left"),
+	move_pane("l", "Right"),
+	{
+		-- When we push LEADER + R...
+		key = "r",
+		mods = "LEADER",
+		-- Activate the `resize_panes` keytable
+		action = wezterm.action.ActivateKeyTable({
+			name = "resize_panes",
+			-- Ensures the keytable stays active after it handles its
+			-- first keypress.
+			one_shot = false,
+			-- Deactivate the keytable after a timeout.
+			timeout_milliseconds = 1000,
+		}),
+	},
+}
+
+config.key_tables = {
+	resize_panes = {
+		resize_pane("j", "Down"),
+		resize_pane("k", "Up"),
+		resize_pane("h", "Left"),
+		resize_pane("l", "Right"),
+	},
+}
+
+-- and finally, return the configuration to wezterm
+return config
