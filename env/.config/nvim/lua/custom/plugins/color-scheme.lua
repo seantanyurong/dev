fe2099d7d517808d52d26eb8ac08c1313e641495
Sean Tan <seantanyurong@gmail.com>
2025-07-18 17:05:37 +0800

    refactor: shift all modules out

---
+++ env/.config/nvim/lua/custom/plugins/color-scheme.lua
@@ -0,0 +1,16 @@
+return {
+  'folke/tokyonight.nvim',
+  priority = 1000, -- Make sure to load this before all the other start plugins.
+  config = function()
+    ---@diagnostic disable-next-line: missing-fields
+    require('tokyonight').setup {
+      transparent = true,
+      styles = {
+        comments = { italic = false }, -- Disable italics in comments
+      },
+    }
+
+    -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+    vim.cmd.colorscheme 'tokyonight-night'
+  end,
+}
