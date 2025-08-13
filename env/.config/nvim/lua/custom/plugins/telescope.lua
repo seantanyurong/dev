39d1d054e7e01a5a85928a34178a0361440e857d
seantanyurong <seantanyurong@gmail.com>
2025-08-13 12:38:17 +0800

    feature: add search for hidden files

--- env/.config/nvim/lua/custom/plugins/telescope.lua
+++ env/.config/nvim/lua/custom/plugins/telescope.lua
@@ -71,6 +71,9 @@ return { -- Fuzzy Finder (files, lsp, etc)l
     vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
     vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
     vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+    vim.keymap.set('n', '<leader>si', function()
+      builtin.find_files { find_command = { 'rg', '--files', '--hidden', '-g', '!.git' } }
+    end, { desc = '[S]earch [I]nvisible Files' })
     vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
     vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
     vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
