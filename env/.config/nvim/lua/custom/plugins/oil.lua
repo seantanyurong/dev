ac97fcfe24f7136f930ad78075b8daaa04fed0f9
seantanyurong <seantanyurong@gmail.com>
2025-12-02 16:20:13 +0800

    feat: copy relative path

--- env/.config/nvim/lua/custom/plugins/oil.lua
+++ env/.config/nvim/lua/custom/plugins/oil.lua
@@ -7,6 +7,26 @@ return {
     view_options = {
       show_hidden = true,
     },
+    keymaps = {
+      ['yp'] = {
+        desc = 'Copy relative filepath to system clipboard',
+        callback = function()
+          local oil = require 'oil'
+          local entry = oil.get_cursor_entry()
+          if not entry then
+            return
+          end
+          local dir = oil.get_current_dir()
+          if not dir then
+            return
+          end
+          local full = vim.fs.joinpath(dir, entry.name)
+          local rel = vim.fn.fnamemodify(full, ':.')
+          vim.fn.setreg('+', rel)
+          vim.notify('Copied: ' .. rel)
+        end,
+      },
+    },
   },
   dependencies = { 'nvim-tree/nvim-web-devicons' }, -- use if you prefer nvim-web-devicons
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
