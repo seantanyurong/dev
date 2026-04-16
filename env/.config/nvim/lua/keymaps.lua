bf389ccb7e1846d9c3bbd0f5a5858cefd9f821eb
seantanyurong <seantanyurong@gmail.com>
2026-04-16 16:20:26 +0800

    feat: remove unused commands

--- env/.config/nvim/lua/keymaps.lua
+++ env/.config/nvim/lua/keymaps.lua
@@ -4,12 +4,7 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setqflist, { desc = 'Open diagnostic [Q]uickfix list' })
 
--- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
--- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- is not what someone will guess without a bit more experience.
---
--- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
--- or just use <C-\><C-n> to exit terminal mode
+-- Exit terminal mode for builtin terminal
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- Keybinds to make split navigation easier.
@@ -29,9 +24,6 @@ vim.keymap.set('n', '<leader>o', '<cmd>Oil<cr>', { desc = 'Open file explorer' }
 
 -- Toggleterm
 vim.keymap.set('n', '<leader>t', '<cmd>ToggleTerm<cr>', { desc = 'Open file explorer' })
-
--- NvimTree
-vim.keymap.set('n', '<leader>f', '<cmd>NvimTreeToggle<cr>', { desc = 'Toggle file explorer' })
 
 -- Git
 vim.keymap.set('n', '<leader>g', '<cmd>Git<cr>', { desc = 'Open git panel' })
