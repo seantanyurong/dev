fb2f6b38f82468eb7ba7f224aab8dfe93a5a7688
seantanyurong <seantanyurong@gmail.com>
2026-06-01 13:53:23 +0800

    feat: enable shift windows keymap

--- env/.config/nvim/lua/keymaps.lua
+++ env/.config/nvim/lua/keymaps.lua
@@ -34,7 +34,7 @@ vim.keymap.set('n', '<leader>e', function()
 end, { desc = 'Error message' })
 
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
--- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
--- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
--- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
--- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
+vim.keymap.set('n', '<C-S-h>', '<C-w>H', { desc = 'Move window to the left' })
+vim.keymap.set('n', '<C-S-l>', '<C-w>L', { desc = 'Move window to the right' })
+vim.keymap.set('n', '<C-S-j>', '<C-w>J', { desc = 'Move window to the lower' })
+vim.keymap.set('n', '<C-S-k>', '<C-w>K', { desc = 'Move window to the upper' })
