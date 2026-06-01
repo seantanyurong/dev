109676b2de56f6720730fe9270c0785393f1a46c
seantanyurong <seantanyurong@gmail.com>
2026-06-01 12:45:03 +0800

    fix: wezterm term info

--- env/.wezterm.lua
+++ env/.wezterm.lua
@@ -6,6 +6,8 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
+config.term = "wezterm"
+
 config.color_scheme = "Catppuccin Macchiato"
 
 config.font = wezterm.font("JetBrains Mono")
